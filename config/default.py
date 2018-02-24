328fcd1c54f8a76cddb83bf57b3d176cc25a5e33
Phillip Malboeuf <phil@boeuf.coffee>
2018-02-24 15:48:03 -0500

    20MB content length

--- config/default.py
+++ config/default.py
@@ -5,7 +5,7 @@ from celery.schedules import crontab
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'DEVELOPMENT').upper()
 DEBUG = ENVIRONMENT == 'DEVELOPMENT'
 TIMEZONE = 'US/Eastern'
-MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+MAX_CONTENT_LENGTH = 20 * 1024 * 1024
 
 LANGS = os.getenv('LANGS', 'fr,es,kr,ch,ja')
 LANGS = LANGS.split(',')
